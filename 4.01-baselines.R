c482be7abf143f57e85501d142791fba756d7074
Derek Wilson <derekw@derekw.org>
2020-02-18 19:28:00 +0000

    Initial commit

---
+++ 4.01-baselines.R
@@ -0,0 +1,56 @@
+
+library(forecast)
+
+mae  <- function(obs, pred) mean(abs(obs - pred), na.rm=TRUE)
+rmse <- function(obs, pred) sqrt(mean((obs - pred)^2, na.rm=TRUE))
+mape <- function(obs, pred) mean(abs((obs - pred) / (obs + 1)) * 100, na.rm=TRUE)
+
+
+# Mean temperature
+mean.temp <- mean(weather.proph$y, na.rm=TRUE)
+rmse(weather.proph$y, mean.temp)
+# [1] 64.46543
+mae(weather.proph$y, mean.temp)
+# [1] 52.62924
+mape(weather.proph$y, mean.temp)
+# [1] 249.9146
+
+
+# Persistent temperature
+weather.proph[, yshift:=shift(y)]
+rmse(weather.proph$y, weather.proph$yshift)
+# [1] 6.265516
+mae(weather.proph$y, weather.proph$yshift)
+# [1] 4.128627
+mape(weather.proph$y, weather.proph$yshift)
+# [1] 9.493788
+
+
+
+# Bit more than 1st year of data
+weather.proph.small <- head(weather.proph$y, 20000)
+
+date() # approx 15 mins :-(
+e.ses <- tsCV(weather.proph.small, ses, h=1)
+date()
+sqrt(mean(e.ses^2, na.rm=TRUE))
+# [1] 6.052966
+mean(abs(e.ses), na.rm=TRUE)
+# [1] 4.031792
+mape(weather.proph.small, weather.proph.small + e.ses)
+# [1] 9.811932
+
+
+date() # approx 1 hour 2 mins :-(
+e.holt <- tsCV(weather.proph.small, holt, h=1)
+date()
+sqrt(mean(e.holt^2, na.rm=TRUE))
+# [1] 5.617949
+mean(abs(e.holt), na.rm=TRUE)
+# [1] 3.93713
+mape(weather.proph.small, weather.proph.small + e.holt)
+# [1] 10.24852
+
+
+
+
---
+++ 4.02-prophet.R
@@ -0,0 +1,44 @@
+
+
+load("data/CambridgeTemperatureModel.RData")
+library(data.table)
+library(lubridate)
+library(prophet)
+
+weather.proph <- weather.08.08.01.cc[, .(ds=as.POSIXct(timestamp, tz="GMT"), y=temperature)]
+
+date() # approx 7 mins
+m.proph <- prophet(weather.proph, daily.seasonality=TRUE, weekly.seasonality=FALSE, yearly.seasonality=TRUE)
+date()
+
+# All of these take approx 15 mins! WTF?
+#future.proph <- make_future_dataframe(m, periods = 365)                   # 1 year
+#future.proph <- make_future_dataframe(m.proph, periods = 1, freq = "day") # 1 day
+future.proph <- make_future_dataframe(m.proph, periods = 1, freq = 3600)   # 1 hour
+#future.proph <- make_future_dataframe(m.proph, periods = 1, freq = 1)     # 1 sec
+
+date() # approx 15 mins
+forecast.proph <- predict(m.proph, future.proph)
+date()
+
+pro.mod.comps <- prophet_plot_components(m.proph, forecast.proph)
+#dev.off()
+
+
+ifelse(!dir.exists(file.path("figures")), dir.create(file.path("figures")), FALSE)
+
+png("figures/prophet.yearly.component.01.png", units = "in", width = 4, height = 3, res = 600)
+pro.mod.comps[2]
+dev.off()
+
+png("figures/prophet.daily.component.01.png", units = "in", width = 4, height = 3, res = 600)
+pro.mod.comps[3]
+dev.off()
+
+
+cv.proph <- cross_validation(m.proph, initial = '11 years', period = '2 hours', horizon = '2 days')
+cv.proph
+
+
+save.image("data/CambridgeTemperatureModelProphet.RData")
+
