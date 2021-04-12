0b585443735f19af269b4a141a28f0461c05f2f9
Derek Wilson <derekw@derekw.org>
2021-04-12 14:37:28 +0100

    Added section dividers for Computer Lab and NOAA ISD data.

--- 1-load.R
+++ 1-load.R
@@ -3,10 +3,16 @@ library(lubridate)
 library(data.table)
 
 
-#weather.raw <- read.csv("weather-raw.csv")
+#############################################################################################################################################
+# 1. Get Cambridge Computer Lab weather station data
+#    See https://www.cl.cam.ac.uk/research/dtg/weather/
+
 weather.raw <- read.csv("https://www.cl.cam.ac.uk/research/dtg/weather/weather-raw.csv")
 weather.raw.orig <- weather.raw
-colnames(weather.raw) <- c("timestamp", "temperature", "humidity", "dew.point", "pressure", "wind.speed.mean", "wind.bearing.mean", "sunshine", "rainfall", "wind.speed.max")
+weather.cols <- c("timestamp", "temperature", "humidity", "dew.point", "pressure",
+                  "wind.speed.mean", "wind.bearing.mean", "sunshine", "rainfall",
+                  "wind.speed.max")
+colnames(weather.raw) <- weather.cols
 str(weather.raw)
 
 weather.raw <- data.table(weather.raw)
@@ -20,7 +26,8 @@ weather.raw[, secs:=as.numeric(as.difftime(time))]
 
 # From: https://www.cl.cam.ac.uk/research/dtg/weather/
 #   "There is a known issue with the sunlight and rain sensors sometimes over-reporting readings.
-#    We are investigating how best to fix this and we should be able to correct archived records once the problem is resolved."
+#    We are investigating how best to fix this and we should be able to correct archived records
+#    once the problem is resolved."
 # INSTEAD I replace rainfall and/or sunshine with binary - 0 if 0, 1 if > 0
 weather.raw[, rainy:=ifelse(rainfall > 0, 1, 0)]
 weather.raw[, sunny:=ifelse(sunshine > 0, 1, 0)]
@@ -34,8 +41,7 @@ weather.raw[, sunny:=ifelse(sunshine > 0, 1, 0)]
 # Add approx (nearest min) sunrise and sunset
 library(suncalc)
 
-# Not certain about lat & long (used https://www.latlong.net/)
-#   TODO Check daylight saving time
+# TODO Check daylight saving time
 #getSunlightTimes(date=as.Date("2019-03-08"), lat=52.210922, lon=0.091964, keep=c("sunrise", "sunset"))
 #getSunlightTimes(date=as.Date("2019-03-08"), lat=52.210922, lon=0.091964, keep=c("sunrise", "sunset"), tz="GMT")
 
@@ -44,14 +50,28 @@ weather.rise.set <- as.data.table(unique(weather.raw$date))
 colnames(weather.rise.set) <- "date"
 
 # Approx. 15 secs each for next two commands
-weather.rise.set[, sunrise:=round_date(getSunlightTimes(date=date, lat=52.210922, lon=0.091964, keep=c("sunrise", "sunset"))$sunrise, unit="1 minute")]
-weather.rise.set[,  sunset:=round_date(getSunlightTimes(date=date, lat=52.210922, lon=0.091964, keep=c("sunrise", "sunset"))$sunset,  unit="1 minute")]
+weather.rise.set[, sunrise:=round_date(getSunlightTimes(date=date,
+                                                        lat=52.210922,
+                                                        lon=0.091964,
+                                                        keep=c("sunrise", "sunset"))$sunrise,
+                                       unit="1 minute")]
+weather.rise.set[,  sunset:=round_date(getSunlightTimes(date=date,
+                                                        lat=52.210922,
+                                                        lon=0.091964,
+                                                        keep=c("sunrise", "sunset"))$sunset,
+                                       unit="1 minute")]
 
 setkey(weather.raw, "date")
 setkey(weather.rise.set, "date")
 weather.raw <- weather.rise.set[weather.raw]
 str(weather.raw)
 
+
+
+#############################################################################################################################################
+# 2. Get NOAA ISD data for Cambridge Airport weather station
+#    Used for filling missing values in Computer Lab data
+#    See https://www.ncdc.noaa.gov/isd
 
 library(stationaRy)
 
@@ -74,8 +94,8 @@ summary(isd) # Lot of NAs
 basename <- paste0("data/CamAirportISD", format(Sys.time(), "%Y.%m.%d"))
 saveRDS(isd, paste0(basename, ".RData"))
 
-# Remove atmos_pres - all NAs :-(
-# ceil_hgt, visibility - too many NAs :-(
+# Remove atmos_pres - all NAs!
+# ceil_hgt, visibility - too many NAs to deal with for now :-(
 isd <- data.table(isd)
 isd <- isd[, .(time, temp, wd, ws, dew_point, rh)]
 class(isd)
@@ -88,20 +108,19 @@ summary(isd)
 # Remove worst of NAs
 isd.08 <- isd[time >= '2008-07-31 08:00:00',]
 
-# Add missing 30 min times
-missing.30.mins  <- seq(ymd_hm('2008-07-31 07:30'), ymd_hm('2020-12-31 23:00'), by = '60 mins')
+# Add NAs - on 30 mins past each hour
+missing.30.mins  <- seq(ymd_hm('2008-07-31 07:30'), ymd_hm('2020-12-31 23:00'), by='60 mins')
 missing.30.mins.dt <- data.table(time=missing.30.mins, temp=NA, wd=NA, ws=NA, dew_point=NA, rh=NA)
 isd.08.30 <- rbind(isd.08, missing.30.mins.dt)[order(time),]
 isd.08.30.orig <- isd.08.30
 
-# Fill in missing values - simple averages
-#   limit to 6 hours or 12 consecutive observations
-# for (col in c("temp")) {
-
+# Fill in 30 mins NAs and other missing data
+# Use simple averages
+# Limit to 6 hours or 12 consecutive observations
 isd.08.30$row <- 1:nrow(isd.08.30)
-for (col in c("temp","wd","ws","dew_point","rh")) {
+for (col in c("temp", "wd", "ws", "dew_point", "rh")) {
     print(col)
-    isd.08.30[, rle := rleid(get(col))][,missing := max(.N +  1 , 2), by = rle]
+    isd.08.30[, rle := rleid(get(col))][,missing := max(.N +  1 , 2), by=rle]
     yy <- isd.08.30[missing <= 13, ..col][[1]]
     xx <- isd.08.30[missing <= 13, row]
     nas <- isd.08.30[missing <= 13 & is.na(get(col)), row]
